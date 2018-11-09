da1fe221509ac9ac89077e896001aa645b5ed1f4
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2018-11-09 14:20:43 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,7 +34,7 @@ bot.on('message', message => {
         console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /say [<message>] [Succès].");
     }
 
-    // -> Commande /help.
+    // -> Commande /aide.
     
     if (message.content === prefix + "aide"){
         message.delete();
@@ -43,7 +43,7 @@ bot.on('message', message => {
             .setThumbnail("http://image.noelshack.com/fichiers/2018/45/3/1541615757-logo.png")
             .addField("Les commandes :", "─────────────────────")
             .addField("[BOT ExtaziaRP] - Carte d'identité:", "-> /rp-c [<prénom rp>] pour afficher la carte d'identité d'un personnage RP.")
-            .addField("[BOT ExtaziaRP] - L'inventaire:", "-> /rp-i [<prénom rp>] pour afficher la carte l'inventaire d'un personnage RP.")
+            .addField("[BOT ExtaziaRP] - L'inventaire:", "-> /rp-i [<prénom rp>] pour afficher l'inventaire d'un personnage RP.")
             .setFooter("© ExtaziaRP; Développement par SOFTYY - 2018/2019.")
         message.channel.send(aide_embed);
         console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /aide [Succès].");

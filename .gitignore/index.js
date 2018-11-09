69e8cb2e8e39c3a2f558111dfa35c6bd6dfacff6
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2018-11-09 19:42:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,8 +8,6 @@ bot.on('ready', () => {
     console.log("[BOT ExtaziaRP] LOGS -> Le bot est actuellement en ligne sur les serveurs discord. [Succès]");
 });
 
-bot.login(process.env.TOKEN);
-
     // -> Rejoindre / quitter le serveur.
 
 bot.on("guildMemberAdd", member => {
@@ -32,6 +30,56 @@ bot.on('message', message => {
         message.delete();
         message.channel.send(say);
         console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /say [<message>] [Succès].");
+    }
+
+    // Sondage RP - 15h00.
+
+    if (message.content === prefix + "sondagerp-15h") {
+        if(message.author.id == "406124485612142592"){
+            message.delete();
+            var sondagerp15h_embed = new Discord.RichEmbed()
+                .setDescription("Sondage :")
+                .addField("Seras-tu disponible à la session RP de 15h00 ?!", "Répondre avec :white_check_mark: ou :negative_squared_cross_mark: (ci-dessous).")
+                .setColor('#CC2EFA')
+                .setFooter("© ExtaziaRP; Développement par SOFTYY - 2018/2019.")
+                .setTimestamp()
+            message.channel.send(sondagerp15h_embed)
+            .then(function (message) {
+                message.react("❎")
+                message.react("✅")
+                console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /sondagerp-15h [Succès].");
+            }).catch(function() {
+            });
+        }else{
+            message.delete();
+            console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur n'ayant pas la permission demande la commande -> /sondagerp-15h [Succès].");
+        }
+
+    }
+
+    // Sondage RP - 21h00.
+
+    if (message.content === prefix + "sondagerp-21h") {
+        if(message.author.id == "406124485612142592"){
+            message.delete();
+            var sondagerp21h_embed = new Discord.RichEmbed()
+                .setDescription("Sondage :")
+                .addField("Seras-tu disponible à la session RP de 21h00 ?!", "Répondre avec :white_check_mark: ou :negative_squared_cross_mark: (ci-dessous).")
+                .setColor('#CC2EFA')
+                .setFooter("© ExtaziaRP; Développement par SOFTYY - 2018/2019.")
+                .setTimestamp()
+            message.channel.send(sondagerp21h_embed)
+            .then(function (message) {
+                message.react("❎")
+                message.react("✅")
+                console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /sondagerp-21h [Succès].");
+            }).catch(function() {
+            });
+        }else{
+            message.delete();
+            console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur n'ayant pas la permission demande la commande -> /sondagerp-15h [Succès].");
+        }
+
     }
 
     // -> Commande /aide.
@@ -88,3 +136,5 @@ bot.on('message', message => {
     }
 
 });
+
+bot.login(process.env.TOKEN);

9271ba7e629546bd17bf321bd122657e01c4c5ad
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-12-27 17:56:36 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,7 +7,7 @@ var prefix = ("/");
 
 bot.on('ready', () => {
     bot.user.setPresence({ game: { name: 'ExtaziaRP.', type : 0}})
-    console.log("[BOT ExtaziaRP v2 - LSPD ] LOGS -> Le bot est actuellement en ligne sur les serveurs discord. [Succès]");
+    console.log("[BOT - EXT ] LOGS -> Le bot est actuellement en ligne sur les serveurs discord. [Succès]");
 });    
 
 bot.on('message', message => {
@@ -15,13 +15,13 @@ bot.on('message', message => {
     let args = message.content.slice(prefix.length).trim().split(' ');
     let command = args.shift().toLowerCase();
     
-    // -> Commande /lspd.
+    // -> Commande /ext.
 
-    if (command === 'lspd') {
+    if (command === 'ext') {
         let say = args.join(' ');
         message.delete();
         message.channel.send(say);
-        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /lspd [<message>] [Succès].");
+        console.log("[BOT] LOGS -> Un utilisateur demande la commande -> /ext [<message>] [Succès].");
     }
 
     

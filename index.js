82c40530febc60db8d44e3ccce072d7a586a1255
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2020-07-05 22:25:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+  
 const Discord = require('discord.js');
 
 var bot = new Discord.Client();
@@ -6,25 +7,27 @@ var prefix = ("/");
     // -> Logs d'informations.
 
 bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: 'Counter Stike: Global Offensive', type : 0}})
-    console.log("[BOT - EXT ] LOGS -> Le bot est actuellement en ligne sur les serveurs discord. [Succès]");
-});    
+    bot.user.setPresence({ game: { name: 'ExtaziaRP.', type : 0}})
+    console.log("[BOT ReLoad] LOGS -> Le bot est actuellement en ligne sur les serveurs discord. [Succès]");
+});
 
 bot.on('message', message => {
     
     let args = message.content.slice(prefix.length).trim().split(' ');
     let command = args.shift().toLowerCase();
-    
-    // -> Commande /ext.
 
-    if (command === 'ext') {
-        let say = args.join(' ');
-        message.delete();
-        message.channel.send(say);
-        console.log("[BOT] LOGS -> Un utilisateur demande la commande -> /ext [<message>] [Succès].");
+    // -> Commande /annonce.
+
+    if (command === 'annonce') {
+        if(message.author.id == "406124485612142592"){
+            let say = args.join(' ');
+            message.delete();
+            message.channel.send(say);
+            console.log("[BOT ReLoad] LOGS -> Un utilisateur demande la commande -> /annonce [<message>] [Succès].");
+        }
     }
-
     
 });
+
 
 bot.login(process.env.TOKEN);

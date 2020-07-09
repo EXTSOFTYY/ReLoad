5c175d1aa848f697db310d0696c42114421fed43
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2020-07-09 22:03:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
   
 const Discord = require('discord.js');
+const MIN_INTERVAL = 2000;
 
 var bot = new Discord.Client();
 var prefix = ("/");
@@ -10,6 +11,11 @@ bot.on('ready', () => {
     bot.user.setPresence({ game: { name: 'ReLoad™', type : 0}})
     console.log("[BOT ReLoad] LOGS -> Le bot est actuellement en ligne sur les serveurs discord. [Succès]");
 });
+
+setInterval(function(){
+    var staffChannel = bot.channels.get("729969260075548739");
+    staffChannel.send("/vote") ;
+}, MIN_INTERVAL);
 
 bot.on('message', message => {
     
